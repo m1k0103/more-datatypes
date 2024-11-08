323f2042b64de703f027357b718665f87ea0889f
miki <84585119+miko1034@users.noreply.github.com>
2024-11-08 13:04:56 +0000

    added tests for queue

--- tests.py
+++ tests.py
@@ -1,9 +1,9 @@
-from more_datatypes import Stack
+from more_datatypes import Stack, Queue
 
-# tests stack length
+# check stack length
 assert Stack(5).max_size == 5
 
-# tests stack functions
+# check stack functions
 s = Stack(2)
 assert s.push("value") == True # if pushed correctly
 assert s.pop() == True # if pops correctly
@@ -18,3 +18,15 @@ assert Stack(4).push(None) == True # if None tries to get pushed, should still w
 
 ### Queue
 
+# check queue length
+assert Queue(5).max_length == 5
+
+#check stack funcs
+q = Queue(2)
+assert q.enqueue("a") == True
+assert q.dequeue() == True
+
+q.enqueue("a")
+q.enqueue("b")
+assert q.enqueue("c") == False # if queue is full
+assert Queue(5).dequeue() == False # if queue is empty
