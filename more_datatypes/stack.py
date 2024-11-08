14be7dbc4bbe18176efd2565a6e2452c60597646
miki <84585119+miko1034@users.noreply.github.com>
2024-11-08 12:36:32 +0000

    stack finished

--- more_datatypes/stack.py
+++ more_datatypes/stack.py
@@ -1,10 +1,31 @@
+import logging
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)
+
 class Stack:
-    def __init__(self,size):
-        self.stack = [None] * size 
-    
+    def __init__(self,max_size):
+        self.stack = [] 
+        self.max_size = max_size
+
     def push(self,value):
-        pass
+        if len(self.stack) == self.max_size:
+            logging.info(f"Push of value {value} failed.")
+            return False
+        else:
+            logger.info(f"Pushing value: {value}")
+            self.stack.append(value)
+            return True
+
     def pop(self):
-        pass
+        if len(self.stack) == 0:
+            logger.info(f"Popping failed.")
+            return False
+        else:
+            logger.info(f"Popping value {self.stack[-1:]}")
+            del self.stack[-1:]
+            return True
+
     def display(self):
-        pass
+        print(self.stack)
+        return
