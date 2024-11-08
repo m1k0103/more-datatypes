4176376320a655f6683fda75b18f6e525ea2f70b
miki <84585119+miko1034@users.noreply.github.com>
2024-11-08 11:46:51 +0000

    started datatyles module

---
+++ more_datatypes/stack.py
@@ -0,0 +1,10 @@
+class Stack:
+    def __init__(self,size):
+        self.stack = [None] * size 
+    
+    def push(self,value):
+        pass
+    def pop(self):
+        pass
+    def display(self):
+        pass
