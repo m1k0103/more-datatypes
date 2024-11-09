07c2260c62caa59431964ffd18354aa862228ecc
mikok <84585119+miko1034@users.noreply.github.com>
2024-11-09 22:34:01 +0000

    progresson queue

--- more_datatypes/queue.py
+++ more_datatypes/queue.py
@@ -3,12 +3,24 @@ class Queue:
         self.max_length = max_length
         self.queue = [None * max_length]
         self.front = 0
-        self.back = 1
+        self.back = 0
     
-    def enqueue(self):
-        if self.back == self.front:
-            pass
+    def enqueue(self,data):
+        if self.back == self.front and self.back == 0: # if empty
+            if self.back == self.max_length:
+                self.back = 0
+                self.queue[self.back] = data
+
+            else:
+                self.queue[self.back] = data
+                self.back += 1
+        elif self.back == self.front and #uhhhhhh too late for this
         pass
 
     def dequeue(self):
+        if self.back == self.front and  # bla bla bla logic 
         pass
+
+    def display(self):
+        print(self.queue)
+        return
